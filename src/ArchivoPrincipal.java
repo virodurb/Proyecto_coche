a99ce99587acbcc6ae5ad5f7011745d31d5a0c2d
Virginia <vrodurb160@g.educaand.es>
2024-11-26 09:52:44 +0100

    Rama mejoras

---
+++ src/ArchivoPrincipal.java
@@ -0,0 +1,10 @@
+public class ArchivoPrincipal {
+    public static void main(String[] args) {
+        Coche coche = new Coche("Toyota", "Corolla", 2020);
+        Cliente cliente = new Cliente("Juan Pérez", "123456789");
+        System.out.println(coche);
+        System.out.println(cliente);
+    }
+}
+
+
